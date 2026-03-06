d235e27a501cf20b048096bd373cbeaded00a316
unknown <sarathymanoj86@gmail.com>
2026-03-06 18:22:35 +0530

    project1

---
+++ sample.py
@@ -0,0 +1,38 @@
+# print("Hello, Welcome To Python Class")
+# print("...........................")
+# username = "sarathy"
+# exp = 10
+# is_active = True
+# value = 12.00
+# print("username: ",username)
+# print("exp: ",exp)
+# print("is_active: ",is_active)
+# print("value: ",value)
+# print("...........................")
+# print(type(username))
+# print(type(exp))
+# print(type(is_active))
+# print(type(value))
+# print("...........................")
+
+# name = input("What is ur Name: ")
+# print(name)
+
+# num1 = int(input("please enter num1: "))
+# num2 = int(input("please enter num2: "))
+# print("addition of nums is       ", num1+num2)
+# print("subtraction of nums is    ", num1-num2)
+# print("multiplication of nums is ", num1*num2)
+# print("division of nums is       ", num1/num2)
+# print("modulus of nums is        ", num1%num2)
+
+# print("...........................")
+
+# a = int(input("Enter password: "))
+# b = 1234 
+# if(a == b):
+#     print("password is correct")
+# else:
+#     print("Incorrect password")
+
+# {.....................day 5......................}
